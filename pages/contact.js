c6dd548899f3235736ccbaed127cfae45077db69
Nachat Ayoub <venomx2020x@gmail.com>
2022-07-10 18:53:14 +0100

    new commit

--- pages/contact.js
+++ pages/contact.js
@@ -4,12 +4,12 @@ const contact = () => {
       <h1 className="text-4xl text-white font-bold mb-4"> Contact Me 😁 </h1>
       <div className="w-full flex justify-center items-center">
         <input
-          className="w-1/2 px-8 py-4 m-2 text-lg font-quick  rounded font-bold placeholder-secondary"
+          className="w-1/2 px-2 md:px-8 py-2 md:py-4 m-2 text-lg font-quick  rounded font-bold placeholder-secondary"
           type="text"
           placeholder="Name"
         />
         <input
-          className="w-1/2 px-8 py-4 m-2 text-lg font-quick rounded font-bold placeholder-secondary focus:font-quick"
+          className="w-1/2 px-2 md:px-8 py-2 md:py-4 m-2 text-lg font-quick rounded font-bold placeholder-secondary focus:font-quick"
           type="email"
           placeholder="Email"
         />
@@ -17,14 +17,14 @@ const contact = () => {
 
       <div className="w-full flex justify-center items-center">
         <input
-          className="w-full px-8 py-4 m-2 text-lg font-quick rounded font-bold placeholder-secondary"
+          className="w-full px-2 md:px-8 py-2 md:py-4 m-2 text-lg font-quick rounded font-bold placeholder-secondary"
           type="text"
           placeholder="Subject"
         />
       </div>
       <div className="w-full flex justify-center items-center">
         <textarea
-          className="w-full px-8 py-4 m-2 text-lg font-quick rounded font-bold placeholder-secondary"
+          className="w-full px-2 md:px-8 py-2 md:py-4 m-2 text-lg font-quick rounded font-bold placeholder-secondary"
           placeholder="Message"
           cols="30"
           rows="6"
